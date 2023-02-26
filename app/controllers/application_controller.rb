b33f9c08d3eec55668bd842b785ba497a164858d
hashihana <120104605+hashihana@users.noreply.github.com>
2023-02-26 14:57:32 +0000

    [Update]新規会員登録画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_sign_up_params, only: [:create]
+
+  protected
+  
+  #def configure_sign_up_params
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:])
+  #end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
 end
