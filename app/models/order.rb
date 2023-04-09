49f6aafecc06a9f7bab5d7f3f6b41d402bcf7401
hashihana <120104605+hashihana@users.noreply.github.com>
2023-04-09 14:58:57 +0000

    [Update]ヘッダー

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,9 +5,10 @@ class Order < ApplicationRecord
   has_many :items, through: :order_details
   
   
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
-
 
 end
