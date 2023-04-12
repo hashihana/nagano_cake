3f0bb1033164db3cc96995ad947a86784e0b04bc
hashihana <120104605+hashihana@users.noreply.github.com>
2023-04-12 15:51:47 +0000

    [Fix]ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+   # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
   namespace :admin do
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
     resources :customers, only:[:index, :new, :create, :show, :edit, :update]
@@ -20,18 +34,7 @@ resources :customers do
    
 end
 
- # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
