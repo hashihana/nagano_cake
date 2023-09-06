231731e9204f8e47b4c46d7049abedfa1a492a8a
hashihana <120104605+hashihana@users.noreply.github.com>
2023-09-06 15:17:03 +0000

    [Fix]public_orders

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def top
     @orders = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
@@ -7,11 +7,11 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-    
+
   def index
     @items = Item.all.page(params[:page]).per(10)
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -21,7 +21,7 @@ class Admin::ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
@@ -30,19 +30,19 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       flash[:notice] = "投稿の更新に成功しました"
-    redirect_to admin_product_path(@product)
+    redirect_to admin_item_path(@item)
     else
       render "show"
     end
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :image, :genre_id, :introduction, :price)
   end
